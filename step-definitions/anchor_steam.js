796b3a421cf4ff249638e9a8ae8519628a06f33f
tomasmansson <tomas.mansson93@gmail.com>
2019-05-21 17:52:41 +0200

    adding 3 last steps

--- step-definitions/anchor_steam.js
+++ step-definitions/anchor_steam.js
@@ -26,8 +26,23 @@ module.exports = function () {
         await sleep(sleepTime)
     });
 
-    this.When(/^I click on search$/, function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
+    this.When(/^I click on search$/, async function () {
+        let searchButton = await $('#ProductSearchSubmitButton > i')
+        await searchButton.click()
+        await sleep(sleepTime)
     });
+
+    this.When(/^I select the search result: anchor steam beer$/, async function () {
+        let searchResultButton = await $('.row-container.clearfix')
+        await searchResultButton.click()
+        await sleep(sleepTime * 6)
+    });
+
+    this.Then(/^the product description should contain the specific text we want$/, async function () {
+        let beerInfo = await $('div.product-details.options-0')
+        let text = await beerInfo.getText()
+        assert (text.includes('Maltig, fruktig smak med inslag av torkade aprikoser'), 'the product description is not containing the text that we wanted')
+      });
+
+
 }
