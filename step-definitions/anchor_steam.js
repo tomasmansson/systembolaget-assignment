2d5f7174497973a3237733ffe2e1998ad9ddb17d
tomasmansson <tomas.mansson93@gmail.com>
2019-05-21 11:16:56 +0200

    Creating first step-definition

---
+++ step-definitions/anchor_steam.js
@@ -0,0 +1,5 @@
+// 1) Sök efter ”anchor steam” på Systembolagets webbplats.
+// Klicka på resultatet av sökningen. 
+// Kontrollera att den beskrivande texten innehåller: 
+// ”Maltig, fruktig smak med inslag av torkade aprikoser”.
+
