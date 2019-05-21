996d12ab23dc3f126a2dc603572eb104beef4184
tomasmansson <tomas.mansson93@gmail.com>
2019-05-21 11:54:29 +0200

    First two steps, Given and When

--- step-definitions/anchor_steam.js
+++ step-definitions/anchor_steam.js
@@ -3,14 +3,31 @@
 // Kontrollera att den beskrivande texten innehåller: 
 // ”Maltig, fruktig smak med inslag av torkade aprikoser”.
 
-let { $,  } = require('./funcs')
+let { $, sleep } = require('./funcs')
 
 let sleepTime = 500
+
+let search = 'anchor steam'
 
 module.exports = function () {
 
 
+    this.Given(/^that web page is loaded$/, async function () {
+        await helpers.loadPage('https://www.systembolaget.se/')
+        await sleep(sleepTime)
+        let ageChoice = await $('#modal-agecheck > div > div > div > div > div.content > div.actions.ng-scope > button')
+        await ageChoice.click()
+        await sleep(sleepTime)
+    });
 
+    this.When(/^I enter anchor steam in the search field$/, async function () {
+        let inputSearch = await $('#ProductSearchTextInput')
+        inputSearch.sendKeys(search)
+        await sleep(sleepTime)
+    });
 
-
+    this.When(/^I click on search$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+    });
 }
