05f25636293d3467c2151aa04ecd0f3c9f102112
tomasmansson <tomas.mansson93@gmail.com>
2019-05-21 11:29:52 +0200

    Adding sleep function

--- step-definitions/funcs.js
+++ step-definitions/funcs.js
@@ -1,14 +1,20 @@
-// Detta är våran gamla func $
+// Function for selecting and returning element or elements by.css
 
 async function $(selector, chosenDriver = driver) {
     let elements = await chosenDriver.findElements(by.css(selector));
     if (elements.length === 0) {
-      return null;
+        return null;
     }
     if (elements.length === 1) {
-      return elements[0];
+        return elements[0];
     }
     return elements;
-  }
-  
-  module.exports.$ = $;
+}
+
+module.exports.$ = $;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports.sleep = sleep;
