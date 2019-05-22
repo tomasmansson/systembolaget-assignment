8bab439eeb22a6c683a146b26262613a0ae8cf82
tomasmansson <tomas.mansson93@gmail.com>
2019-05-22 12:57:06 +0200

    Ballast almost done

--- step-definitions/ballast.js
+++ step-definitions/ballast.js
@@ -10,14 +10,32 @@ module.exports = function () {
 
   this.When(/^I enter 'ballast' in the search field$/, async function () {
     let inputSearch = await $('#ProductSearchTextInput')
-        inputSearch.sendKeys(search)
-        await sleep(sleepTime)
+    inputSearch.sendKeys(search)
+    await sleep(sleepTime)
   });
 
 
-  this.Then(/^it should show how many articles there is in the sortiment based on the search$/, function () {
-    
+  this.Then(/^it should show how many articles there is in the assortment based on the search$/, async function () {
+    let amountAssortment = await driver.findElement(by.css('li.all-hits.selected > a > span:nth-child(3)')).getText()
+    await amountAssortment
+    console.log(amountAssortment)
+
   });
 
+
+  this.Then(/^it should show how many articles there is at retailers based on the search$/, async function () {
+    let retailersButton = await $('li.store-hits > a')
+    await retailersButton.click()
+    await sleep(sleepTime)
+
+    let amountRetailers = await driver.findElement(by.css('li.store-hits.selected > a > span:nth-child(2)')).getText()
+    await amountRetailers
+    console.log(amountRetailers)
+
+  });
+
+  this.Then(/^it should report the quantities back to me$/, function () {
+
+  });
 
 }
