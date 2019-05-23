6b416c2561ff0bb14f977205dddc8481ff824b45
tomasmansson <tomas.mansson93@gmail.com>
2019-05-23 08:45:37 +0200

    declaring variables outside function

--- step-definitions/ballast.js
+++ step-definitions/ballast.js
@@ -6,6 +6,9 @@ let sleepTime = 500
 
 let search = 'ballast'
 
+let amountRetailers
+let amountAssortment
+
 module.exports = function () {
 
   this.When(/^I enter 'ballast' in the search field$/, async function () {
@@ -16,25 +19,24 @@ module.exports = function () {
 
 
   this.Then(/^it should show how many articles there is in the assortment based on the search$/, async function () {
-    let amountAssortment = await driver.findElement(by.css('li.all-hits.selected > a > span:nth-child(3)')).getText()
+    amountAssortment = await driver.findElement(by.css('li.all-hits.selected > a > span:nth-child(3)')).getText()
     await amountAssortment
-    console.log(amountAssortment)
 
   });
-
 
   this.Then(/^it should show how many articles there is at retailers based on the search$/, async function () {
     let retailersButton = await $('li.store-hits > a')
     await retailersButton.click()
     await sleep(sleepTime)
 
-    let amountRetailers = await driver.findElement(by.css('li.store-hits.selected > a > span:nth-child(2)')).getText()
+    amountRetailers = await driver.findElement(by.css('li.store-hits.selected > a > span:nth-child(2)')).getText()
     await amountRetailers
-    console.log(amountRetailers)
 
   });
 
   this.Then(/^it should report the quantities back to me$/, function () {
+    console.log(amountAssortment)
+    console.log(amountRetailers)
 
   });
 
